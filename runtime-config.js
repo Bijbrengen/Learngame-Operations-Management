db17437047e7bb72869d298ce8e0833c8426953d
Bijbrengen <slagter@bijbrengen.nl>
2026-07-27 16:53:44 +0200

    Update runtime-config.js met slimme hostdetectie en HTTPS tunnel URL

--- runtime-config.js
+++ runtime-config.js
@@ -1,6 +1,14 @@
-window.LEARNGAME_OM_CONFIG = Object.freeze({
-  "apiBase": "http://127.0.0.1:47111/api",
-  "appUrl": "http://127.0.0.1:47113/",
-  "dashboardUrl": "http://127.0.0.1:47112/",
-  "editorUrl": "http://127.0.0.1:47114/"
-});
+(function() {
+  var isLocal = typeof window !== "undefined" && (
+    window.location.hostname === "127.0.0.1" ||
+    window.location.hostname === "localhost"
+  );
+  var tunnelUrl = "https://slimy-masks-glow.loca.lt/api";
+
+  window.LEARNGAME_OM_CONFIG = Object.freeze({
+    "apiBase": isLocal ? "http://127.0.0.1:47111/api" : tunnelUrl,
+    "appUrl": isLocal ? "http://127.0.0.1:47113/" : "https://bijbrengen.github.io/Learngame-Operations-Management/",
+    "dashboardUrl": isLocal ? "http://127.0.0.1:47112/" : "https://bijbrengen.github.io/LeerpretDashboard/",
+    "editorUrl": "http://127.0.0.1:47114/"
+  });
+})();
